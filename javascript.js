42839453fdfa2e3b2e2257ee3da420a04c2955e0
infamous-gezeus <150240116+infamous-gezeus@users.noreply.github.com>
2024-03-15 01:35:36 -0400

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-function login() {
-  if ($("#username").val() == "admin" && $("#password").val() == "password") {
-    alert("You are a valid user");
-  } else {
-    alert("You are not a valid user");
-  }
-}
